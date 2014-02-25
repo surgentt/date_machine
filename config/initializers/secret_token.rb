19d19aee339119460ae3444ac45a06268c8f4377
den1k <den1k@me.com>
2014-02-25 18:57:14 -0500

    rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DateMachine::Application.config.secret_key_base = '75e475823ed85d393df482bd2c82b0e6dc695d5adf9381c809cc5f0e60accb2493dbdc05605343293094ddf1da54e6baecc23da6310c6ca2c7818f185eac731b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DateMachine::Application.config.session_store :cookie_store, key: '_date_machine_session'
