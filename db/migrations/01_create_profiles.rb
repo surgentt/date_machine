1575ee130c890e01a31077897e3fb02a505b821c
Thomas Surgent <surgentt@gmail.com>
2014-02-25 20:01:39 -0500

    development database good

--- db/migrations/01_create_profiles.rb
+++ db/migrations/01_create_profiles.rb
@@ -1,5 +1,9 @@
-class CreateProfiles < ActiveRecord:Migration
-
-
-
+class CreateProfiles < ActiveRecord::Migration
+  def change 
+    create_table :profiles do |t|
+      t.string :username
+      t.boolean :messanged?
+      t.integer :match_score
+    end
+  end
 end
