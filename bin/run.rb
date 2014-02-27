be043acc99382d8ecb23ca0bde8613d975bba5d6
Thomas Surgent <surgentt@gmail.com>
2014-02-27 09:07:46 -0500

    good to present

--- bin/run.rb
+++ bin/run.rb
@@ -7,16 +7,12 @@ interact_session = OkcupidDriver.new(session)
 
 interact_session.login
 
-def like_people
-  4.times do 
-    interact_session.quickmatch_like_people
-  end
-  interact_session.add_liked_to_profile_array
-end
 
-def message
+  # 4.times do 
+  #   interact_session.quickmatch_like_people
+  # end
+  # interact_session.add_liked_to_profile_array
+
+
   interact_session.get_unmessaged_profiles_from_db
   interact_session.message
-end
-
-
